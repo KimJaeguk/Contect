4cca8697d2255749d82fad28ee9f35867f8f3bbb
KimJaeguk <kempire902@gmail.com>
2024-05-23 16:39:42 +0900

    contact db

--- app/src/main/java/com/example/contect/MainActivity.kt
+++ app/src/main/java/com/example/contect/MainActivity.kt
@@ -5,21 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.contect.ui.theme.ContectTheme
-import java.util.jar.Attributes.Name
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,31 +32,49 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainScreen() {
-    var name by remember {
-        mutableStateOf("")
-    }
-    var number by remember {
-        mutableStateOf("")
-    }
-    var email by remember {
-        mutableStateOf("")
-    }
-    Column {
-        OutlinedTextField(value = name, onValueChange = { name = it })
-        OutlinedTextField(value = number, onValueChange = { number = it })
-        OutlinedTextField(value = email, onValueChange = { email= it })
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "등록")
+    @Preview(showBackground = true)
+    @Composable
+    fun MainScreen() {
+        val context = LocalContext.current
+        val db = remember {
+            AppDatabase.getDatabase(context)
         }
-        Text(text = "이름: $name")
-        Text(text = "연락처: $number")
-        Text(text = "이메일: $email")
+        Column {
+            var name by remember { mutableStateOf("") }
+            var phoneNumber by remember { mutableStateOf("") }
+            var email by remember { mutableStateOf("") }
+            OutlinedTextField(value = name, onValueChange = { name = it })
+            OutlinedTextField(value = phoneNumber, onValueChange = { phoneNumber = it })
+            OutlinedTextField(value = email, onValueChange = { email = it })
+
+            val rememberCoroutineScope = rememberCoroutineScope()
+            Button(onClick = {
+                rememberCoroutineScope.launch(Dispatchers.IO) {
+                    db.userDao().insertAll(
+                        User(
+                            username = name,
+                            phoneNumber = phoneNumber,
+                            email = email
+                        )
+                    )
+                }
+
+            }) {
+                Text(text = "등록")
+            }
+            Column {
+                val userlist by db.userDao().getAll().collectAsState(initial = emptyList())
+                userlist.forEach {
+                    Text(text = "이름: ${it.username}")
+                    Text(text = "연락처: ${it.phoneNumber}")
+                    Text(text = "이메일: ${it.email}")
+                }
+                for (user in userlist) {
+                Text(text = "이름: ${user.username}")
+                Text(text = "연락처: ${user.phoneNumber}")
+                Text(text = "이메일: ${user.email}")
+            }
+            }
+        }
     }
 }
-
-
--- app/src/main/java/com/example/contect/User.kt
+++ app/src/main/java/com/example/contect/User.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class User(
-    @PrimaryKey val uid: Int,
-    @ColumnInfo(name = "name") val firstName: String?,
-    @ColumnInfo(name = "phone_number") val lastName: String?,
+    @PrimaryKey(autoGenerate = true) val uid: Int= 0,
+    @ColumnInfo(name = "name") val username: String?,
+    @ColumnInfo(name = "phoneNumber") val phoneNumber: String?,
     @ColumnInfo(name = "email") val email: String?
 )
